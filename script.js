bbfaf22de0e3ddd279abbef7eee87bd41b53560c
Raditoiu Karina <raditoiukarina@gmail.com>
2023-09-26 17:27:30 +0300

    initial setup

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    // we create the square elemens dynamically
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    //if the mouse is over the square, we call the setColor function
+    square.addEventListener('mouseover', () => setColor(square))
+    
+    //if the mouse is out of the square, we call the removeColor function
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+   const color = getRandomColor()
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
